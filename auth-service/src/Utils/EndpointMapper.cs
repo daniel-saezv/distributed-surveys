d12d4537cba7ca16ee7ce8c6a302bcb0254ee73d
daniel.saezv <gamu.development@gmail.com>
2025-06-01 02:53:02 +0200

    organize endpoint mapping and create unit test project

---
+++ auth-service/src/Utils/EndpointMapper.cs
@@ -0,0 +1,61 @@
+using AuthService.Infra.Context;
+using AuthService.Infra.Models;
+using AuthService.Mappers;
+using AuthService.Models.Login;
+using AuthService.Models.Register;
+using AuthService.Services.Jwt;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthService.Utils;
+
+/// <summary>
+/// Utility class for mapping endpoints in the Auth Service.
+/// </summary>
+public static class EndpointMapper
+{
+    /// <summary>
+    /// Maps the endpoints for user registration and login in the Auth Service.
+    /// </summary>
+    /// <param name="app"></param>
+    public static WebApplication MapEndpoints(this WebApplication app)
+    {
+        app.MapPost("/register", async Task<Results<Created<RegisterResponse>, ProblemHttpResult>> (
+            RegisterRequest req,
+            AuthDbContext db,
+            IPasswordHasher<User> hasher) =>
+        {
+            var exists = await db.Users.AnyAsync(u => u.Username == req.Username);
+            if (exists)
+                return ProblemResults.AlreadyRegistered();
+
+            var createdUser = await db.Users.AddAsync(req.ToUser(hasher));
+            var response = createdUser.Entity.ToResponse();
+
+            await db.SaveChangesAsync();
+
+            return TypedResults.Created($"/users/{response.Id}", response);
+        });
+
+        app.MapPost("/login", async Task<Results<Ok<LoginResponse>, UnauthorizedHttpResult>> (
+            LoginRequest req,
+            AuthDbContext db,
+            IPasswordHasher<User> hasher,
+            IJwtTokenGenerator jwtGen) =>
+        {
+            var user = await db.Users.SingleOrDefaultAsync(u => u.Username == req.Username);
+            if (user is null)
+                return TypedResults.Unauthorized();
+
+            var result = hasher.VerifyHashedPassword(user, user.PasswordHash, req.Password);
+            if (result is not PasswordVerificationResult.Success)
+                return TypedResults.Unauthorized();
+
+            var token = jwtGen.GenerateToken(user);
+            return TypedResults.Ok(new LoginResponse(token));
+        });
+
+        return app;
+    }
+}
---
+++ auth-service/tests/AuthService.UnitTests/Login/LoginTests.cs
@@ -0,0 +1,10 @@
+namespace AuthService.UnitTests.Login;
+
+public class LoginTests
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
